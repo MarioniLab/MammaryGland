7459af2050b688a48aa1576d50c3b2f95f1d6011
Karsten <karsten_bach@hotmail.com>
2017-03-20 07:25:08 +0000

    updates on figures

---
+++ src/Figure2.R
@@ -0,0 +1,241 @@
+library(plyr)
+library(splines)
+library(destiny)
+library(dplyr)
+library(reshape)
+library(ggplot2)
+library(cowplot)
+library(viridis)
+library(RColorBrewer)
+source("functions.R")
+
+dataList <- readRDS("../data/Robjects/ExpressionList_Clustered.rds")
+m <- dataList[[1]]
+pD <- dataList[[2]]
+fD <- dataList[[3]]
+
+pD$Condition <- mapvalues(pD$Condition, from=c("V","P","L","I"),
+			  to=c("Virgin", "Pregnancy",
+			       "Lactation", "Post-Involution"))
+
+# Cells
+keepCells <- pD$PassAll & !pD$isImmuneCell & !pD$isOutlier 
+m <- m[,keepCells]
+pD <- pD[keepCells,]
+
+pD$cluster <- mapvalues(pD$cluster, from=c(1,2,3,4,5,6,7,9,10),
+			  to=c(1,2,3,4,5,6,7,8,9))
+#Normalize
+m.norm <- t(t(m)/pD$sf)
+rownames(m.norm) <- fD$symbol
+
+
+##########################################
+#
+#
+# Diffusion Maps
+#
+#
+##########################################
+
+
+condComb <- c("Virgin","Pregnancy")
+
+# For Basal and Luminal cells
+#
+#
+
+rnd_seed <- 300
+keepCells <- pD$Condition %in% condComb
+m.vp <- m[,keepCells]
+pD.vp <- pD[keepCells,]
+
+#Gene filtering
+keep <- rowMeans(m.vp)>0.1
+m.vp <- m.vp[keep,]
+fD.vp <- fD[keep,]
+
+#
+m.norm <- t(t(m.vp)/pD.vp$sf)
+
+brennecke <- BrenneckeHVG(m.norm,suppress.plot=TRUE)
+fD.vp$highVar <- fD.vp$id %in% brennecke
+
+exps <- m.norm[fD.vp$highVar,]
+exps <- t(log(exps+1))
+dm <- DiffusionMap(exps,n_eigs=20,k=50)
+library(scatterplot3d)
+pal <- brewer.pal(n=9,name="Paired")
+cols <- mapvalues(pD.vp$cluster,levels(pD.vp$cluster)[-1],
+		  pal)
+dms <- eigenvectors(dm)[,1:3]
+
+## 3d Plot for Basal cells
+scatterplot3d(dms[,2],-dms[,1],dms[,3],color=cols,pch=20,angle=20,
+	      cex.symbols=1.5,
+	      mar=c(5,3,-0.1,3)+0.1,
+	      xlab="DC2",
+	      ylab="DC1",
+	      zlab="DC3",
+	      box=FALSE)
+
+## Extract as grob for plot
+library(gridGraphics)
+library(gridExtra)
+grab_grob <- function(){
+  grid.echo()
+  grid.grab()
+}
+
+g <- grab_grob()
+g <- grid.arrange(g)
+
+#####################################
+# Only luminal cells in P and V
+#
+#####################################
+
+#Pre-Filtering before DE-Analysis
+# Cells
+keepCells <- !(pD$cluster %in% c(6,7,9)) & pD$Condition %in% condComb
+m.vp <- m[,keepCells]
+pD.vp <- pD[keepCells,]
+
+#Gene filtering
+keep <- rowMeans(m.vp)>0.1
+m.vp <- m.vp[keep,]
+fD.vp <- fD[keep,]
+
+#
+m.norm <- t(t(m.vp)/pD.vp$sf)
+
+brennecke <- BrenneckeHVG(m.norm,suppress.plot=TRUE)
+fD.vp$highVar <- fD.vp$id %in% brennecke
+
+
+# Compute Diffusion map
+set.seed(rnd_seed)
+exps <- m.norm[fD.vp$highVar,]
+exps <- t(log(exps+1))
+dm <- DiffusionMap(exps,n_eigs=20,k=50)
+# plot(eigenvalues(dm)[1:20])
+
+# Pseudotime and branching
+set.seed(rnd_seed)
+dcs <- eigenvectors(dm)[,1:2]
+t1 <- which.min(dcs[,2])
+t2 <- which.min(dcs[,1])
+t3 <- which.max(dcs[,1])
+dpt <- DPT(dm, branching=TRUE, tips=c(t1,t2,t3))
+root <- which(dpt@tips[,1])[3]
+rootdpt <- paste0("DPT",root)
+# plot(dpt,pch=20,dcs=c(1,2),col_by=rootdpt)
+
+# Rename branches
+branch <- dpt@branch[,1]
+branch[is.na(branch)]  <- "Decision stage"
+branch[branch==1] <- "Root"
+branch[branch==2] <- "Secretory lineage"
+branch[branch==3] <- "Hormone-sensing lineage"
+pD.vp$branch <- factor(branch,levels=c("Root","Decision stage",
+				    "Secretory lineage",
+				    "Hormone-sensing lineage"))
+pD.vp$dpt<- dpt[["dpt"]]
+pD.vp$DC1 <- eigenvectors(dm)[,1]
+pD.vp$DC2 <- eigenvectors(dm)[,2]
+
+clusts <- as.numeric(as.character(sort(unique(pD.vp$cluster))))
+cols <- brewer.pal(n=9,name="Paired")[clusts]
+names(cols) <- clusts
+
+
+## Plots
+p0 <- ggplot(pD.vp, aes(x=DC1,y=-DC2, color=cluster)) +
+    geom_point(size=2, pch=20) +
+    guides(colour = guide_legend(override.aes = list(size=3))) +
+    scale_color_manual(values=cols)+
+    guides(colour=FALSE)
+
+pal <- brewer.pal(n=9,name="Paired")[c(1,2,3,5,6,7,8)]
+forcLeg <- filter(pD, !cluster %in% c(9,4))
+clustLeg <- ggplot(forcLeg, aes(x=tSNE1,y=tSNE2,color=cluster)) +
+    geom_point() +
+    scale_color_manual(values=pal) +
+    theme(legend.direction="horizontal",
+	  legend.title=element_blank(),
+	  legend.position="bottom")+
+    guides(colour=guide_legend(nrow=1,
+			       override.aes=list(size=3)))
+
+clustLeg <- get_legend(clustLeg)
+    
+
+p <- ggplot(pD.vp, aes(x=DC1,y=-DC2, color=dpt)) +
+    geom_point(size=1, pch=20) +
+    scale_color_viridis(option="magma",begin=1,end=0) +
+    theme(legend.direction="horizontal")
+
+
+# Aldehyde trend
+ald <- filter(fD.vp, symbol %in% c("Aldh1a3")) %>% .$id
+fDC2Trend <- data.frame("DC2"=-pD.vp$DC2,
+		   "Aldh1a3"=unname(log2(t(m.norm)[,ald]+1)))
+DC2Trend <- ggplot(fDC2Trend, aes(x=DC2, y=Aldh1a3)) +
+    geom_point(size=0.5,color="#7570b3",alpha=0.5) +
+    geom_smooth(method="loess",color="#7570b3") +
+    #     geom_ribbon(aes(ymin = 0,ymax = predict(loess(Aldh1a3 ~ DC2 ))),
+    #                     alpha = 0.3,fill = 'dodgerblue')+
+#     ggtitle("Progenitor marker expression") +
+    ylab("Log-Expression") +
+    xlab("") +
+    theme(axis.line.y=element_blank(),
+	  axis.line.x=element_line(colour="black"),
+	  axis.text.y=element_blank(),
+	  axis.ticks.y=element_blank()
+	  ) +
+    coord_flip()
+
+# Csn2 and Esr1 trend
+genes <- filter(fD.vp, symbol %in% c("Csn2","Foxa1")) %>% .$id
+fDC1Trend <- data.frame("DC1"=pD.vp$DC1,
+		   "Csn2"=unname(log2(t(m.norm)[,genes[1]]+1)),
+		   "Foxa1"=unname(log2(t(m.norm)[,genes[2]]+1)),
+		   "Aldh1a3"=NA)
+fDC1Trend <- melt(fDC1Trend,id="DC1",variable_name="Gene")
+DC1Trend <- ggplot(fDC1Trend, aes(x=DC1, y=value, color=Gene)) +
+    geom_point(size=0.5,alpha=0.5) +
+    geom_smooth(method="glm",formula="y~ns(x,df=5)",
+		method.args=list(family="quasipoisson")) +
+    theme(axis.line.x=element_blank(),
+	  axis.line.y=element_line(colour="black"),
+	  axis.text.x=element_blank(),
+	  axis.ticks.x=element_blank()
+	  ) +
+    scale_color_manual(values=c("#1b9e77","#d95f02","#7570b3")) +
+    ylab("Log-Expression") +
+    guides(color=guide_legend(override.aes=list(fill=NA,size=2))) +
+    theme(legend.direction="vertical",legend.title=element_blank()) +
+    xlab("") 
+
+leg1 <- get_legend(p)
+p <- p %+% guides(colour=FALSE)
+
+leg2 <- get_legend(DC1Trend)
+
+DC1Trend <- DC1Trend %+% guides(colour=FALSE)
+
+subP <- plot_grid(p,DC2Trend,DC1Trend,align="hv",rel_widths=c(1,0.6),
+		  rel_heights=c(1,0.6))
+
+subP1 <- subP + draw_grob(leg1,0.7,-0.01,1/3,0.5) + draw_grob(leg2,0.7,-0.11,1/3,0.5)
+subP1 <- plot_grid(NULL,subP1,NULL,labels=c("","c",""),nrow=1,
+		   rel_widths=c(0.2,1,0.2))
+
+
+subPab <-plot_grid(g,p0,labels=c("a","b",nrow=2)) 
+subP0 <- plot_grid(subPab,clustLeg,nrow=2,rel_heights=c(1,0.1))
+
+
+cairo_pdf("Figure2.pdf",width=12.41,height=15.54)
+plot_grid(subP0,subP1,labels=c("",""),nrow=2)
+dev.off()
---
+++ src/Figure3.R
@@ -0,0 +1,308 @@
+library(plyr)
+library(cowplot)
+library(pheatmap)
+library(reshape2)
+library(splines)
+library(dplyr)
+library(destiny)
+library(ggplot2)
+source("functions.R")
+library(RColorBrewer)
+library(lmtest)
+
+#REad in data
+rnd_seed <- 300
+dataList <- readRDS("../data/Robjects/ExpressionList_Clustered.rds")
+m <- dataList[[1]]
+pD <- dataList[[2]]
+fD <- dataList[[3]]
+
+#load transcription factors
+tfCheck <- read.table("../data/miscData/TFcheckpoint_WithENSID.tsv",
+		header=TRUE, sep="\t")
+
+tfs <- filter(fD, id %in% tfCheck$ensembl_gene_id) %>% .$symbol
+
+# Pre-Filtering  selecting only virgin and pregnancy
+# Cells
+keepCells <- pD$PassAll & !pD$isImmuneCell & !pD$isOutlier & !(pD$cluster %in% c(6,7,10)) &
+    pD$Condition %in% c("V","P")
+m <- m[,keepCells]
+pD <- pD[keepCells,]
+
+#Gene filtering
+keep <- rowMeans(m)>0.1
+m <- m[keep,]
+fD <- fD[keep,]
+
+#Normalize
+m.norm <- t(t(m)/pD$sf)
+
+# Selection of HVGs
+brennecke <- BrenneckeHVG(m.norm,suppress.plot=TRUE)
+fD$highVar <- fD$id %in% brennecke
+
+#Expression matrix for DiffusionMap
+exps <- m.norm[fD$highVar,]
+exps <- t(log(exps+1))
+
+# Compute Diffusion map
+set.seed(rnd_seed)
+dm <- DiffusionMap(exps,n_eigs=20,k=50)
+plot(eigenvalues(dm)[1:20])
+
+# Pseudotime and branching
+set.seed(rnd_seed)
+dcs <- eigenvectors(dm)[,1:2]
+t1 <- which.min(dcs[,2])
+t2 <- which.min(dcs[,1])
+t3 <- which.max(dcs[,1])
+dpt <- DPT(dm, branching=TRUE, tips=c(t1,t2,t3))
+root <- which(dpt@tips[,1])[3]
+rootdpt <- paste0("DPT",root)
+plot(dpt,pch=20,dcs=c(1,2),col_by=rootdpt)
+
+# Rename branches
+branch <- dpt@branch[,1]
+branch[is.na(branch)]  <- "Decision stage"
+branch[branch==1] <- "Root"
+branch[branch==2] <- "Secretory lineage"
+branch[branch==3] <- "Hormone-sensing lineage"
+pD$branch <- factor(branch,levels=c("Root","Decision stage",
+				    "Secretory lineage",
+				    "Hormone-sensing lineage"))
+pD$dpt<- dpt[["dpt"]]
+pD$DC1 <- eigenvectors(dm)[,1]
+pD$DC2 <- eigenvectors(dm)[,2]
+
+
+#Sanity checks
+#PDT is correlated with real time
+boxp <- ggplot(pD, aes(SampleID, dpt,fill=Condition)) +
+    geom_boxplot() +
+    theme_bw(base_size=14)
+
+# Branches
+branchPlot <- ggplot(pD, aes(DC1,DC2, color=branch)) +
+    geom_point() +
+    theme_bw(base_size=14)
+
+
+#Branch interpretation follwos dpt
+boxp2 <- ggplot(pD, aes(branch, dpt)) +
+    geom_boxplot() +
+    theme_bw(base_size=14)
+
+########################
+#
+# Hormone-sensing lineage
+#
+########################
+lineages <- c("Hormone-sensing lineage",
+	      "Secretory lineage")
+out <- list()
+for (lin in lineages) {
+    pD.sub <- pD[pD$branch %in% c("Root","Decision stage", 
+			      lin),]
+    pD.sub$DPTRank <- rank(pD.sub$dpt, ties.method="first")
+    m.sub <- m.norm[,as.character(pD.sub$barcode)]
+    keep <- rowMeans(m.sub) > 0.1
+    m.sub <- m.sub[keep,]
+    fD.sub <- fD[keep,]
+    ids <- colnames(pD.sub)
+    yhet <- data.frame(t(m.sub))
+    stopifnot(identical(rownames(m.sub),as.character(fD.sub$id)))
+    colnames(yhet) <- fD.sub$symbol
+    yhet$barcode <- colnames(m.sub)
+    fullDat <- join(pD.sub,yhet, by="barcode")
+
+    res <- data.frame()
+    genes <- fD.sub$symbol
+    m.smooth <- matrix(nrow=length(genes),ncol=nrow(fullDat))
+    colnames(m.smooth) <- as.character(fullDat$barcode)
+    rownames(m.smooth) <- genes
+    # take m.smooth het to allow allocation of vectors for columns
+    m.smooth <- t(m.smooth)
+    for (gene in genes) {
+	x <- fullDat$dpt
+	y <- log2(fullDat[,gene]+1)
+	mod1 <- lm(y ~ ns(x,df=3))
+	lmmod <- lm(y~x)
+
+	# F-test only on alternative model
+	fstat <- summary(mod1)$fstatistic
+	cfs <- mod1$coefficients
+	names(cfs) <- paste0("c",c(0:(length(cfs)-1)))
+	## Create P-Value and coefficient matrix
+	p <- pf(fstat[1],df1=fstat[2],df2=fstat[3], lower.tail=FALSE)
+	gradient <- lmmod$coefficients[2]
+
+	tmp <- data.frame(Gene=gene,
+			  PValue=p,
+			  gradient=gradient)
+	tmp <- cbind(tmp,t(cfs))
+	res <- rbind(res,tmp)
+	##Update fitted Value gene expression matrix
+	m.smooth[,gene] <- mod1$fitted.values
+    }
+    m.smooth <- t(m.smooth)
+    maxExp <-apply(m.smooth,1,max)
+    tooLow <- names(maxExp[maxExp<0.5]) 
+
+
+    res$PAdjust<- p.adjust(res$PValue, method="bonferroni")
+    res <- filter(res,!Gene %in% tooLow)
+
+    ## Heatmap of top500
+    deGenes <- as.character(res$Gene[order(res$PValue)][1:500])
+
+
+    #Prepare heatmap
+    ord <- arrange(pD.sub, DPTRank) %>% .$barcode %>% as.character()
+    m.smooth.ord <- m.smooth[deGenes,ord]
+    rownames(m.sub) <- fD.sub$symbol
+    m.sub.ord <- log2(m.sub[deGenes,ord]+1)
+    m.smooth.ord <- m.smooth.ord/apply(m.smooth.ord,1,max)
+
+    pAll <- pheatmap(m.smooth.ord,
+	 cluster_cols=FALSE,
+	 show_colnames=FALSE,
+	 clustering_distance_rows="euclidean",
+	 clustering_method="average",
+	 treeheight_row=0,
+	 legend=FALSE,
+	 show_rownames=FALSE,
+	 fontsize=6)
+
+    ## Heatmap of all TF 
+    deGenes <- as.character(res$Gene[res$PAdjust< 0.001])
+    deGenes <- deGenes[deGenes %in% tfs]
+
+    ord <- arrange(pD.sub, DPTRank) %>% .$barcode %>% as.character()
+    m.smooth.ord <- m.smooth[deGenes,ord]
+    rownames(m.sub) <- fD.sub$symbol
+    m.sub.ord <- log2(m.sub[deGenes,ord]+1)
+    m.smooth.ord <- m.smooth.ord/apply(m.smooth.ord,1,max)
+    di <- dist(m.smooth[deGenes,])
+    cl <- hclust(di)
+    cl <- cutree(cl,k=4)
+
+    pTf <- pheatmap(m.smooth.ord,
+	 cluster_cols=FALSE,
+	 show_colnames=FALSE,
+	 clustering_distance_rows="euclidean",
+	 treeheight_row=0,
+	 clustering_method="average",
+	 show_rownames=TRUE,
+	 fontsize=6)
+
+    if (lin=="Hormone-sensing lineage") {
+    features <- c("Jun","Krt8","Igfbp5")
+    lables <- c("a","b","c")
+    } else{
+    features <- c("Ogfrl1","Cdkn3","Glycam1")
+    lables <- c("d","e","f")
+    }
+
+    plList <- list()
+    for (feature in features) {
+    forPlot <- fullDat[,c("DPTRank","dpt","barcode","cluster",feature)]
+    forPlot <- melt(forPlot,id=c("barcode","DPTRank","dpt","cluster"))
+    p <- ggplot(forPlot, aes(x=dpt, y=log2(value+1))) +
+	geom_point(size=0.7) +
+	geom_smooth(method="lm",formula="y~ns(x,df=3)") +
+	xlab("DPT Rank") +
+	ylab("Log-Expression") +
+	ggtitle(feature) 
+    plList[[feature]] <- p
+    }
+    expPlot <- plot_grid(plotlist=plList,nrow=1)
+
+    subP1 <- plot_grid(expPlot,pTf[[4]],nrow=2,labels=c(lables[2],lables[3]),rel_heights=c(0.5,1),
+		       vjust=0.5,scale=0.95)
+    fullP <- plot_grid(pAll[[4]],subP1,ncol=2,labels=c(lables[1],""),
+		       vjust=0.5)
+    out[[lin]] <- fullP
+}
+
+dev.off()
+cairo_pdf("Figure3dpt.pdf",width=12.41,height=17.54)
+plot_grid(plotlist=out,nrow=2)
+dev.off()
+
+
+########################
+#
+# Two Branch comparison
+#
+########################
+
+pD.sub <- pD[(!pD$branch %in% c("Root","Decision stage")),] 
+pD.sub <- group_by(pD.sub, branch) %>%
+    mutate(dptNorm=dpt/max(dpt)) %>%
+    ungroup()
+			      
+m.sub <- m.norm[,as.character(pD.sub$barcode)]
+keep <- rowMeans(m.sub) > 0.01
+m.sub <- m.sub[keep,]
+fD.sub <- fD[keep,]
+ids <- colnames(pD.sub)
+yhet <- data.frame(t(m.sub))
+stopifnot(identical(rownames(m.sub),as.character(fD.sub$id)))
+colnames(yhet) <- fD.sub$symbol
+yhet$barcode <- colnames(m.sub)
+fullDat <- join(pD.sub,yhet, by="barcode")
+
+res <- data.frame()
+genes <- fD.sub$symbol
+m.smooth <- matrix(nrow=length(genes),ncol=nrow(fullDat))
+colnames(m.smooth) <- as.character(fullDat$barcode)
+rownames(m.smooth) <- genes
+# take m.smooth het to allow allocation of vectors for columns
+m.smooth <- t(m.smooth)
+for (gene in genes) {
+    x <- fullDat$dptNorm
+    y <- log2(fullDat[,gene]+1)
+    fctr <- fullDat$branch
+    mod0 <- lm(y ~ ns(x,df=5))
+    mod1 <- lm(y ~ ns(x,df=5)*fctr)
+    lrt <- lrtest(mod0,mod1)
+
+    ## Create P-Value and coefficient matrix
+    p <- lrt[5][2,]
+    
+    tmp <- data.frame(Gene=gene,
+		      PValue=p)
+    res <- rbind(res,tmp)
+    ##Update fitted Value gene expression matrix
+    #     m.smooth[,gene] <- mod1$fitted.values
+}
+# m.smooth <- t(m.smooth)
+
+res$PAdjust<- p.adjust(res$PValue, method="bonferroni")
+
+deGenes <- as.character(res$Gene[res$PAdjust< 0.001])
+deGenes <- arrange(res, PValue) %>% .$Gene %>% .[1:10]
+# deGenes <- deGenes[deGenes %in% tfs]
+
+
+#Prepare heatmap
+ord <- arrange(pD.sub, DPTRank) %>% .$barcode %>% as.character()
+m.smooth.ord <- m.smooth[deGenes,ord]
+rownames(m.sub) <- fD.sub$symbol
+m.sub.ord <- log2(m.sub[deGenes,ord]+1)
+m.smooth.ord <- m.smooth.ord/apply(m.smooth.ord,1,max)
+
+pheatmap(m.smooth.ord,
+	 cluster_cols=FALSE,
+	 show_colnames=FALSE,
+	 show_rownames=FALSE)
+
+features <- as.character(deGenes)
+forPlot <- fullDat[,c("dptNorm","barcode","branch",features)]
+forPlot <- melt(forPlot,id=c("barcode","dptNorm","branch"))
+ggplot(forPlot, aes(x=dptNorm, y=log2(value+1),lty=branch)) +
+    geom_point() +
+    geom_smooth(method="lm",formula="y~ns(x,df=5)",aes(color=NULL)) +
+    facet_wrap(~variable,scales="free") +
+    theme_bw()
