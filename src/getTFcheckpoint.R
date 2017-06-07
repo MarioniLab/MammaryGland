792c556b277de7fba7521aeb9e185ac0eea37326
Karsten <karsten_bach@hotmail.com>
2017-06-07 15:03:52 +0100

    switched to org.Mm

--- src/getTFcheckpoint.R
+++ src/getTFcheckpoint.R
@@ -1,7 +1,7 @@
 # Scritp to download and format TFcheckpoint database
 
 library(dplyr)
-library(biomaRt)
+library(org.Mm.eg.db)
 
 # ---- Download ----
 download.file("http://www.tfcheckpoint.org/data/TFCheckpoint_download_180515.txt", destfile="../data/miscData/TFcheckpoint.txt")
@@ -13,19 +13,15 @@ tfcheckpoint <- read.csv("../data/miscData/TFcheckpoint.txt", sep="\t", row.name
 tfcheckpoint <- tfcheckpoint[tfcheckpoint$entrez_mouse!=0,]
 tfcheckpoint <- tfcheckpoint[tfcheckpoint$DbTF!=0,]
 
-# ---- formating ----
-# Add column with ENS ID
-ensembl <- useMart(biomart="ensembl", dataset="mmusculus_gene_ensembl")
 
-filt <- tfcheckpoint$entrez_mouse
-res <- getBM(mart=ensembl,
-	     filters="entrezgene",
-	     values=filt,
-	     attributes=c("entrezgene","ensembl_gene_id"))
+# ---- formating ----
+ids <- as.character(tfcheckpoint$entrez_mouse)
+res <- select(org.Mm.eg.db, keys=ids, column="ENSEMBL")
+res$ENTREZID <- as.numeric(res$ENTREZID)
 
 
 colnames(res) <- c("entrez_mouse","ensembl_gene_id")
-tfcheckpoint.withENSG <- full_join(res,tfcheckpoint,by="entrez_mouse")
+tfcheckpoint.withENSG <- full_join(res,tfcheckpoint, by="entrez_mouse")
 
 # Save
 write.table(tfcheckpoint.withENSG,"../data/miscData/TFcheckpoint_WithENSID.tsv",row.names=FALSE,quote=FALSE,
