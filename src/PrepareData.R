1ac8e832ee20e95bb32f273036c30159bf79aa58
Karsten <karsten_bach@hotmail.com>
2017-06-23 15:36:49 +0100

    bugFix!

--- src/PrepareData.R
+++ src/PrepareData.R
@@ -9,17 +9,22 @@ library(dplyr)
 gene_bc_matrix <- load_cellranger_matrix("../data/CellRangerData/secondRun_2500/MammaryGland",
 					 genome="mm10")
 pDat <- data.frame(pData(gene_bc_matrix))
-fDat <- data.frame(fData(gene_bc_matrix))
 cDat <- as.matrix(exprs(gene_bc_matrix))
+
+#reduce size of matrix
+keep <- rowSums(cDat) > 1
+cDat <- cDat[keep,]
+fDat <- data.frame(fData(gene_bc_matrix))[rownames(cDat),]
 
 # ---- Formatting ----
 
 # Add more info to phenotype Data
 pDat <- mutate(pDat, SeqID=substr(barcode,18,18)) %>%
-	       mutate(SeqID=mapvalues(SeqID,c("1","2","3","4","5","6","7","8"),
-					 c("A7","B7","C7","D7","E7","F7","G7","H7"))) %>%
+	mutate(barcode=as.character(barcode)) %>%
+        mutate(SeqID=mapvalues(SeqID,c("1","2","3","4","5","6","7","8"),
+				 c("A7","B7","C7","D7","E7","F7","G7","H7"))) %>%
         mutate(SampleID=mapvalues(SeqID,c("A7","B7","C7","D7","E7","F7","G7","H7"),
-				  c("NP1","G2","PI2","G1","L2","L1","NP2","PI1"))) %>%
+				  c("PI1","PI2","L1","L2","G1","G2","NP1","NP2"))) %>%
 	mutate(Replicate=as.factor(substr(SampleID,nchar(SampleID),nchar(SampleID))),
 	       Condition=factor(substr(SampleID,1,nchar(SampleID)-1),levels=c("NP","G","L","PI"))) %>%
 	mutate(SampleID=factor(SampleID,levels=c("NP1","NP2","G1","G2","L1","L2","PI1","PI2"))) 
@@ -29,11 +34,11 @@ mitoGenes <- read.table("../data/miscData/MitoGenes.txt")
 tfCheck <- read.table("../data/miscData/TFcheckpoint_WithENSID.tsv",
 		header=TRUE, sep="\t")
 
-fDat <- mutate(fDat,
-	       Mitochondrial=id %in% mitoGenes$V1,
-	       TranscriptionFactor=id %in% tfCheck$ensembl_gene_id)
+fDat$Mitochondrial <- fDat$id %in% mitoGenes$V1
+fDat$TranscriptionFactor <- fDat$id %in% tfCheck$ensembl_gene_id
 
 
 # Save data
+stopifnot(identical(rownames(fDat),rownames(cDat)) & identical(colnames(cDat),pDat$barcode))
 DataList <- list("phenoData"=pDat, "featureData"=fDat, "counts"=cDat)
 saveRDS(DataList,file="../data/Robjects/secondRun_2500/ExpressionList.rds")
