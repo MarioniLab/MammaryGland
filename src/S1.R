4c71300f9d6c74c98792d91ba786ba3ea36dd72c
Karsten <karsten_bach@hotmail.com>
2017-08-07 15:37:31 +0100

    updated according to text

--- src/S1.R
+++ src/S1.R
@@ -10,14 +10,11 @@ library(cowplot)
 source("functions.R")
 
 # Load Data
-dataList <- readRDS("../data/Robjects/secondRun_2500/ExpressionList.rds")
+dataList <- readRDS("../data/Robjects/secondRun_2500/ExpressionList_QC.rds")
 sumDat <- read.csv("../data/CellRangerData/secondRun_2500/Summary.csv")
 m <- dataList[["counts"]]
 pD <- dataList[["phenoData"]]
 fD <- dataList[["featureData"]]
-
-pD$UmiSums<- colSums(m)
-pD$GenesDetected <- colSums(m!=0)
 
 # Summary table 
 sumry <- group_by(pD, SampleID) %>%
@@ -29,8 +26,24 @@ sumry <- left_join(sumry, sumDat[,c("SampleID","NumberOfReads","Saturation")])
 p1 <- tableGrob(sumry,rows=NULL,)
 
 # Load plots from QCAnalysis
-source("QCAnalysis.R")
-# dev.off()
-# cairo_pdf("../paper/figures/S1.pdf",height=12.41,width=17.54)
+# Illustrate thresholds
+gdHist <- ggplot(pD, aes(x=GenesDetected,y=..density..)) +
+    geom_histogram(fill="white",color="black",bins=100) +
+    scale_x_log10() +
+    xlab("Total number of genes detected") +
+    facet_wrap(~Condition) 
+
+libSizeHist <- ggplot(pD, aes(x=UmiSums,y=..density..)) +
+    geom_histogram(fill="white",color="black",bins=100) +
+    scale_x_log10() +
+    facet_wrap(~Condition) +
+    xlab("Total number of unique molecules") 
+
+cellViability <- ggplot(pD, aes(x=prcntMito, y=GenesDetected, color=Condition, shape=Replicate))+
+    geom_point() +
+    xlab("Percentage of Mitochondrial RNA molecules") +
+    ylab("Total number of genes detected")
+
+cairo_pdf("../paper/figures/S1.pdf",height=12.41,width=17.54)
 plot_grid(p1,gdHist,libSizeHist,cellViability, labels="auto")
-# dev.off()
+dev.off()
