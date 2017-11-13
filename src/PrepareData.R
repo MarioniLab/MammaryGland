b69058a370fe44ddebc3e31a8fd5394e3df3fd33
Karsten <karsten_bach@hotmail.com>
2017-11-13 14:34:43 +0000

    bugfix

--- src/PrepareData.R
+++ src/PrepareData.R
@@ -22,8 +22,8 @@ fDat <- data.frame(fData(gene_bc_matrix))[rownames(cDat),]
 pDat <- mutate(pDat, SeqID=substr(barcode,18,18)) %>%
 	mutate(barcode=as.character(barcode)) %>%
         mutate(SeqID=mapvalues(SeqID,c("1","2","3","4","5","6","7","8"),
-				 c("A7","B7","Myo","D7","E7","F7","G7","H7"))) %>%
-        mutate(SampleID=mapvalues(SeqID,c("A7","B7","Myo","D7","E7","F7","G7","H7"),
+				 c("A7","B7","C7","D7","E7","F7","G7","H7"))) %>%
+        mutate(SampleID=mapvalues(SeqID,c("A7","B7","C7","D7","E7","F7","G7","H7"),
 				  c("PI1","PI2","L1","L2","G1","G2","NP1","NP2"))) %>%
 	mutate(Replicate=as.factor(substr(SampleID,nchar(SampleID),nchar(SampleID))),
 	       Condition=factor(substr(SampleID,1,nchar(SampleID)-1),levels=c("NP","G","L","PI"))) %>%
